25eb6909fdbfa0568f0d318446abe1f8c24720a6
NelsonJ <nelsonjcelisc@gmail.com>
2026-02-12 21:17:17 +0000

    feat: migración a MongoDB, unificación de DTO en common-lib y soporte para tipo de transacción

--- journaler/src/main/java/co/edu/uniandes/matchengine/journaler/repository/JournalRepository.java
+++ journaler/src/main/java/co/edu/uniandes/matchengine/journaler/repository/JournalRepository.java
@@ -1,7 +1,15 @@
 package co.edu.uniandes.matchengine.journaler.repository;
 
 import co.edu.uniandes.matchengine.journaler.model.JournalEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface JournalRepository extends JpaRepository<JournalEntity, Long> {
+public interface JournalRepository extends MongoRepository<JournalEntity, Long> {
 }
+
+// package co.edu.uniandes.matchengine.journaler.repository;
+
+// import co.edu.uniandes.matchengine.journaler.model.JournalEntity;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface JournalRepository extends JpaRepository<JournalEntity, Long> {
+// }
